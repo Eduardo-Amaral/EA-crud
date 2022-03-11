3afa88f62c5d68d643e2272d475e629bd045f64c
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-03-11 11:01:18 -0300

    test1

--- config/db.js
+++ config/db.js
@@ -1,1 +1,5 @@
-module.exports = {mongoURI: 'mongodb+srv://EduardoAmaral:<iQXGxIh67vA2i5Mo>@ea-blog.edbda.mongodb.net/EA-Blog?retryWrites=true&w=majority'}
+// if (process.env.NODE_ENV == 'production'){
+  module.exports = {mongoURI: 'mongodb+srv://EduardoAmaral:B7hTClmdVDHeXfKF@eacrud.edbda.mongodb.net/eacrud?retryWrites=true&w=majority'}
+// }else{
+//   module.exports =  {mongoURI: 'mongodb://localhost/blognodejs'}
+// }
