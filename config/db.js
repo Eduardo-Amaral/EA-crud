107069f54a39d6e5b85e6506e417c7c9725266f5
Eduardo-Amaral <eduardo.lopes.amaral@outlook.com>
2022-03-11 10:15:52 -0300

    config db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://EduardoAmaral:<password>@ea-blog.edbda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
+  module.exports = {mongoURI: 'mongodb+srv://EduardoAmaral:<edugamerbr3>@ea-blog.edbda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
 }else{
   module.exports =  {mongoURI: 'mongodb://localhost/blognodejs'}
 }
